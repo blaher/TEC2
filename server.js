2b66558cf8c294e74becced412487383b6b5255c
Benjamin J. Young <blaher09@gmail.com>
2019-07-11 17:14:22 -0400

    Making project

---
+++ server.js
@@ -0,0 +1,33 @@
+var channel = '#nintendo';
+
+var irc = require('irc');
+var client = new irc.Client('irc.chatspike.net', 'TEC2', {
+    channels: [channel],
+});
+
+client.addListener('message'+channel, function (from, message) {
+  if (message.substring(0, 1) === '!') {
+    var command = message.substring(1);
+    if (command.indexOf(' ') >= 0) {
+      command = command.substring(0, command.indexOf(' '));
+    }
+
+    switch(command) {
+      case 'hello':
+        client.say(channel, 'Hello World!');
+      break;
+
+      case 'strike':
+        client.say(channel, 'That\'s a strike, '+message.substring(message.indexOf(' ')+1)+'!');
+      break;
+
+      case 'rank':
+        client.say(channel, 'Ranks are coming soon.');
+      break;
+    }
+  }
+});
+
+client.addListener('error', function(message) {
+    console.log('error: ', message);
+});

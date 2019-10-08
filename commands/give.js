2b1cccf4335f015ea17838964cf4bbdd05e9ae7c
Benjamin J. Young <blaher09@gmail.com>
2019-10-08 13:57:04 -0400

    Limiting debt on coins being given

--- commands/give.js
+++ commands/give.js
@@ -3,13 +3,20 @@ module.exports = (client, db, channel, from, params) => {
     var nick = params[0];
     var amount = params[1];
 
-    db.Nick.increment('coins', {where: {nick: nick}, by: amount});
-    db.Nick.decrement('coins', {where: {nick: from}, by: amount});
+    db.Nick.findOne({where: {nick: from}}).then(function(dbNick) {
+      if (amount > dbNick.coins) {
+        client.say(channel, 'You don\'t have enough coins, '+from+'.');
+        return;
+      }
 
-    if (amount == 1) {
-      client.say(channel, from+' has given '+nick+' '+amount+' coin.');
-    } else {
-      client.say(channel, from+' has given '+nick+' '+amount+' coins.');
-    }
+      db.Nick.increment('coins', {where: {nick: nick}, by: amount});
+      db.Nick.decrement('coins', {where: {nick: from}, by: amount});
+
+      if (amount == 1) {
+        client.say(channel, from+' has given '+nick+' '+amount+' coin.');
+      } else {
+        client.say(channel, from+' has given '+nick+' '+amount+' coins.');
+      }
+    });
   }
 }
